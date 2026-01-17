486168286aecfaf7cdf1734f539971a60e1aaa0e
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-01-17 22:19:22 +0530

    Finalize mobile UI and hamburger navbar

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ fetch("components/navbar.html")
     document.getElementById("navbar").innerHTML = data;
   })
   .catch(err => console.error("Navbar load error:", err));
+
+  //Phase 2
+  const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  reveals.forEach(section => {
+    const windowHeight = window.innerHeight;
+    const elementTop = section.getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (elementTop < windowHeight - revealPoint) {
+      section.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
